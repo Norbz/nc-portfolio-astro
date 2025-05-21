43eb48eeb96868d18598cd7beba3b21398991ef9
Nicolas Chesné <hello@nicolaschesne.fr>
2025-05-21 14:45:39 +0200

    feat: Adding header feat: Adding Hero feat: Adding activities feat: Adding skills banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+import icon from 'astro-icon';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@includes" as *;`,
+          includePaths: [path.resolve(__dirname, 'src/scss')]
+        }
+      }
+    },
+    resolve: {
+      alias: {
+        '@includes': path.resolve(__dirname, 'src/scss/includes')
+      }
+    },
+    plugins: [],
+  }
+});
